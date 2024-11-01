ed9c16ec0258e23641ec270cacb2a27919e1dc4e
Piero Miranda <142460985+PieroMiranda1@users.noreply.github.com>
2024-10-31 20:18:19 -0500

    chore : initial commit

---
+++ mindflicks-platform/src/main/java/com/acme/mindflicks/platform/MindflicksPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.acme.mindflicks.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindflicksPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindflicksPlatformApplication.class, args);
+    }
+
+}
---
+++ mindflicks-platform/src/test/java/com/acme/mindflicks/platform/MindflicksPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.mindflicks.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindflicksPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
