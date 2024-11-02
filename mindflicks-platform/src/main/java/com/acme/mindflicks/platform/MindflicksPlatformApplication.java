c1fb2a84986a1ab2ab026f0bffe37a4bb4bac582
Gabriel Lapa <u202216831@upc.edu.pe>
2024-11-02 17:19:52 -0500

    feat added EnableJpaAuditing

--- mindflicks-platform/src/main/java/com/acme/mindflicks/platform/MindflicksPlatformApplication.java
+++ mindflicks-platform/src/main/java/com/acme/mindflicks/platform/MindflicksPlatformApplication.java
@@ -2,8 +2,10 @@ package com.acme.mindflicks.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MindflicksPlatformApplication {
 
     public static void main(String[] args) {
